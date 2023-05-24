8a61e3cb64f07607a949356ae6895c25729dae48
yigitaksoy <yigitaksoy@gmail.com>
2023-05-24 16:28:20 +0200

    Configure server and API routes, establish database connection

--- server/index.js
+++ server/index.js
@@ -1,23 +1,30 @@
-const express = require("express");
-const connectToDatabase = require("./config/database");
-
-// Create Express app
-const app = express();
-
-// Load environment variables from .env file
 require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { connectToDatabase } = require("./config/database");
+const apiRoutes = require("./routes/api");
 
-// Connect to the database
-connectToDatabase();
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
-// Routes
+// Connect to the database
+connectToDatabase()
+  .then(() => {
+    console.log("Connected to the database");
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    // API routes
+    app.use("/api", apiRoutes);
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error);
+    process.exit(1);
+  });
