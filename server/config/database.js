340f33e49c26b49ef7ff3d0f9b1d303ebd92c560
yigitaksoy <yigitaksoy@gmail.com>
2023-05-24 16:28:53 +0200

    Fix export of connectToDatabase function

--- server/config/database.js
+++ server/config/database.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
-    try {
-      await mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-  
-      console.log('Connected to the database');
-    } catch (error) {
-      console.error('Database connection error:', error.message);
-      process.exit(1);
-    }
-  };
-  
-  module.exports = connectToDatabase;
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
+    console.error("Database connection error:", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = {
+  connectToDatabase,
+};
