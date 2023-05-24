cc5ace5d2914ef11fd9a2b1eae2db9f970eb951f
yigitaksoy <yigitaksoy@gmail.com>
2023-05-24 15:17:54 +0200

    Add database connection configuration

---
+++ server/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectToDatabase = async () => {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+  
+      console.log('Connected to the database');
+    } catch (error) {
+      console.error('Database connection error:', error.message);
+      process.exit(1);
+    }
+  };
+  
+  module.exports = connectToDatabase;
