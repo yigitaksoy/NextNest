b1e35de79bd48e9aff5f99ea48d063484834015e
yigitaksoy <yigitaksoy@gmail.com>
2023-05-26 11:42:45 +0200

    Refactor listing scraping logic

--- server/services/listingService.js
+++ server/services/listingService.js
@@ -1,22 +1,47 @@
 const puppeteer = require("puppeteer");
-const jsdom = require("jsdom");
 
 const scrapeListings = async (url) => {
-  const browser = await puppeteer.launch({ headless: true });
+  console.log("Scraping listings for URL:", url);
+
+  const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
+
   await page.goto(url, { waitUntil: "domcontentloaded" });
 
-  const htmlString = await page.content();
-  const dom = new jsdom.JSDOM(htmlString);
-  const result = dom.window.document.querySelectorAll(".search-result");
+  const listings = await page.evaluate(() => {
+    const elements = Array.from(document.querySelectorAll(".search-result"));
 
-  const listings = [];
-  for (const element of result) {
-    const href = element?.querySelectorAll("a")?.[0]?.href;
-    listings.push(href);
-  }
+    return elements.map((element) => {
+      const image = element?.querySelector(".search-result-image img")?.src;
+      const title = element
+        ?.querySelector(".search-result__header-title")
+        ?.textContent.trim();
+      const linkElement = element.querySelector(
+        ".search-result-main a[data-object-url-tracking='resultlist']"
+      );
+      const url = linkElement?.getAttribute("href")?.replace(/(\?.*)$/, "");
+      const price = element
+        ?.querySelector(".search-result-price")
+        ?.textContent.trim();
+      const details = Array.from(
+        element?.querySelectorAll(".search-result-kenmerken li")
+      ).map((li) => li.textContent.trim());
+
+      return {
+        image,
+        title,
+        url: `https://www.funda.nl${url}`, // Add base URL to the scraped URL
+        price,
+        details,
+      };
+    });
+  });
 
   await browser.close();
+
+  listings.forEach((listing) => {
+    console.log("Listing URL:", listing.url);
+  });
 
   return listings;
 };
