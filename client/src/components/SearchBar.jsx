7990ff2df957bb87b63d0599f5d0390b3bc6bc5a
yigitaksoy <yigitaksoy@gmail.com>
2023-05-30 10:28:10 +0200

    Update Search bar styles

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,5 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
+import { MapIcon } from "@heroicons/react/24/outline";
+import { EnvelopeIcon } from "@heroicons/react/24/outline";
+import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
 
 const SearchBar = () => {
   const [formData, setFormData] = useState({
@@ -9,7 +12,6 @@ const SearchBar = () => {
     maxPrice: "",
     minSize: "",
     minBedrooms: "",
-    maxAge: "",
     email: "",
   });
 
@@ -18,9 +20,11 @@ const SearchBar = () => {
       ...prevData,
       [e.target.name]: e.target.value,
     }));
-
-    console.log("Form Data:", formData);
   };
+
+  useEffect(() => {
+    console.log("Form Data:", formData);
+  }, [formData]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -41,10 +45,6 @@ const SearchBar = () => {
         }
       );
 
-      console.log("Request Payload:", {
-        ...formData,
-        toEmail: formData.email,
-      });
       console.log(response);
       console.log("Email sent:", response.data);
     } catch (error) {
@@ -53,100 +53,124 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="h-screen w-screen items-center justify-center">
-      <form className="flex flex-row items-center" onSubmit={handleSubmit}>
-        {/* Listing Type */}
-        <div>
-          <select
-            name="listingType"
-            className="select-info select w-full max-w-xs"
-            value={formData.listingType}
-            onChange={handleChange}
-            required
-          >
-            <option disabled value="">
-              Listing Type
-            </option>
-            <option value="huur">huur</option>
-            <option value="koop">koop</option>
-          </select>
-          {/* Location */}
-          <input
-            type="text"
-            name="location"
-            placeholder="City, neighborhood, address, etc. "
-            className="input-bordered input w-full max-w-xs"
-            value={formData.location}
-            onChange={handleChange}
-            required
-          ></input>
-          {/* Min Price */}
-          <input
-            type="text"
-            name="minPrice"
-            placeholder="Min Price"
-            className="input-bordered input w-full max-w-xs"
-            value={formData.minPrice}
-            onChange={handleChange}
-            required
-          />
-          {/* Max Price */}
-          <input
-            type="text"
-            name="maxPrice"
-            placeholder="Max Price"
-            className="input-bordered input w-full max-w-xs"
-            value={formData.maxPrice}
-            onChange={handleChange}
-            required
-          />
-          {/* Min Size */}
-          <input
-            type="text"
-            name="minSize"
-            placeholder="Min Size"
-            className="input-bordered input w-full max-w-xs"
-            value={formData.minSize}
-            onChange={handleChange}
-            required
-          />
-          {/* Min Bedrooms */}
-          <input
-            type="number"
-            name="minBedrooms"
-            placeholder="Min Bedrooms"
-            className="input-bordered input-info input w-full max-w-xs"
-            value={formData.minBedrooms}
-            onChange={handleChange}
-            required
-          />
-          {/* Max Age */}
-          <input
-            type="text"
-            name="maxAge"
-            placeholder="Max Age"
-            className="input-bordered input w-full max-w-xs"
-            value={formData.maxAge}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          {/* Email */}
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            className="input-bordered input w-full max-w-xs"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit" className="btn">
-          Send
-        </button>
-      </form>
+    <div className="">
+      <div className="container mx-auto flex h-screen items-center justify-center p-2 md:p-0">
+        <form onSubmit={handleSubmit}>
+          <div className="grid grid-cols-1 gap-6 rounded-3xl border-4 border-sky-400 bg-gray-100 p-6 shadow-lg">
+            <div className="flex flex-shrink-0">
+              <button className="bg-transparent p-2" type="button">
+                <input
+                  className="hidden"
+                  type="radio"
+                  id="forRent"
+                  value="huur"
+                  name="listingType"
+                  onChange={handleChange}
+                  required
+                />
+                <label
+                  className="btn border-2 bg-transparent px-5 py-2.5 text-black"
+                  htmlFor="forRent"
+                >
+                  For Rent
+                </label>
+              </button>
+              <button className="p-2" type="button">
+                <input
+                  className="hidden"
+                  type="radio"
+                  id="forSale"
+                  value="koop"
+                  name="listingType"
+                  onChange={handleChange}
+                  required
+                />
+                <label
+                  className="btn border-2 bg-transparent px-5 py-2.5 text-black"
+                  htmlFor="forSale"
+                >
+                  For Sale
+                </label>
+              </button>
+            </div>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+              <div className="p-2">
+                <div className="relative">
+                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                    <MapIcon className="h-5 w-5 text-gray-500" />
+                  </div>
+                  <input
+                    type="text"
+                    className="input block w-full rounded-lg border p-2.5 pl-10 text-sm"
+                    placeholder="City, neighborhood, address, etc."
+                    value={formData.location}
+                    name="location"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="grid grid-cols-2 gap-2 p-2">
+                <div className="relative flex items-center">
+                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                    <p>From</p>
+                  </div>
+                  <input
+                    type="number"
+                    id="minPrice"
+                    className="input block w-full rounded-lg border p-2.5 pl-16 text-sm"
+                    placeholder="Price From"
+                    value={formData.minPrice}
+                    name="minPrice"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="relative flex items-center">
+                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                    <p>To</p>
+                  </div>
+                  <input
+                    type="number"
+                    id="maxPrice"
+                    className="input block w-full rounded-lg border p-2.5 pl-10 text-sm"
+                    placeholder="Limit"
+                    value={formData.maxPrice}
+                    name="maxPrice"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-1 p-2">
+              <div className="flex justify-between">
+                <div className="relative flex w-full items-center">
+                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                    <EnvelopeIcon className="h-5 w-5 text-gray-500" />
+                  </div>
+                  <input
+                    type="email"
+                    id="maxPrice"
+                    className="input block w-full rounded-lg border p-2.5 pl-10 text-sm"
+                    placeholder="E-mail"
+                    value={formData.email}
+                    name="email"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-center">
+              <button className="btn w-full rounded-md border bg-gray-800 p-2 text-white lg:w-1/4">
+                Search & Send
+                <PaperAirplaneIcon class=" h-5 w-5 text-white" />
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
