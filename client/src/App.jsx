069e876083f0758187279222b8a0a65e6713145a
yigitaksoy <yigitaksoy@gmail.com>
2023-05-30 15:49:56 +0200

    Add auth context provider and react router routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
-import SearchBar from "./components/SearchBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 import "./assets/css/style.css";
+import Home from "./routes/Home";
+import Register from "./routes/Register";
+import Login from "./routes/Login";
 
-function App() {
+const App = () => {
   return (
-    <div className="h-screen w-screen bg-zinc-100">
-      <SearchBar />
-    </div>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthContextProvider>
   );
-}
+};
 
 export default App;
