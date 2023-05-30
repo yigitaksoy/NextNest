a40ea6a0102fe169903125cedfc1c604c6c08e01
yigitaksoy <yigitaksoy@gmail.com>
2023-05-30 15:48:44 +0200

    Add firebase Auth context

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState, useEffect } from "react";
+import { auth } from "../utils/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
