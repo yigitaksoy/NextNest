31066017b72783fd32dc618f4e051bfb4fc17468
yigitaksoy <yigitaksoy@gmail.com>
2023-05-30 15:50:24 +0200

    Add home route

---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchBar from "../components/SearchBar";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <SearchBar />
+    </div>
+  );
+};
+
+export default Home;
