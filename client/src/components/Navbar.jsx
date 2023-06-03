0c2e1deb3f779d43a83aa68bf43f52a67a59f4df
yigitaksoy <yigitaksoy@gmail.com>
2023-06-03 12:42:17 +0200

    Style logo and logout button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,15 +9,19 @@ const Navbar = () => {
   return (
     <div className="navbar fixed bg-black p-5">
       <div className="flex-1">
-        <p className="text-md btn-ghost btn normal-case text-white">NextNest</p>
+        <p className="btn-ghost btn text-lg font-bold normal-case text-sky-400">
+          NextNest
+        </p>
       </div>
-      <div className="flex-none text-white">
-        Logout
+      <div className="mx-auto text-lg text-gray-300">
         <button
-          className="btn-ghost btn-square btn"
+          className="btn-ghost btn-square btn mx-auto flex"
           onClick={() => signOut(auth)}
         >
-          <ArrowRightOnRectangleIcon className="h-5 w-5 text-white" />
+          <div className="mr-10 flex items-center">
+            <p className="mr-2">Logout</p>
+            <ArrowRightOnRectangleIcon className="h-5 w-5 text-gray-300" />
+          </div>
         </button>
       </div>
     </div>
