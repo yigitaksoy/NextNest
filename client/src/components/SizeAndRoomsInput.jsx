185cd814e7bca56dc2e0aa2310d9650cecdcb839
yigitaksoy <yigitaksoy@gmail.com>
2023-06-15 18:50:22 +0200

    Replace room input with react-select

--- client/src/components/SizeAndRoomsInput.jsx
+++ client/src/components/SizeAndRoomsInput.jsx
@@ -1,10 +1,16 @@
 import Select from "react-select";
 import surface from "../data/surface.json";
+import rooms from "../data/rooms.json";
 
 const SizeAndRoomsInput = ({ handleChange, formData }) => {
-  const options = surface;
-  const selectedSurface = options.find(
+  const surfaceOptions = surface;
+  const roomOptions = rooms;
+
+  const selectedSurface = surfaceOptions.find(
     (option) => option.value === formData.minSize
+  );
+  const selectedRoom = roomOptions.find(
+    (option) => option.value === formData.minBedrooms
   );
 
   const handleSurfaceChange = (selectedOption) => {
@@ -16,12 +22,22 @@ const SizeAndRoomsInput = ({ handleChange, formData }) => {
     };
     handleChange(event);
   };
+
+  const handleRoomChange = (selectedOption) => {
+    const event = {
+      target: {
+        name: "minBedrooms",
+        value: selectedOption ? selectedOption.value : "",
+      },
+    };
+    handleChange(event);
+  };
   return (
     <div className="grid grid-cols-2 gap-2 p-2">
       <div className="relative flex items-center">
         <Select
           id="minSize"
-          options={options}
+          options={surfaceOptions}
           value={selectedSurface}
           isClearable
           isSearchable={false}
@@ -60,15 +76,43 @@ const SizeAndRoomsInput = ({ handleChange, formData }) => {
         />
       </div>
       <div className="relative flex items-center">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"></div>
-        <input
-          type="number"
+        <Select
           id="minBedrooms"
-          className="input block w-full rounded-lg border p-2.5 pl-10 text-sm"
-          placeholder="Bedrooms"
-          value={formData.minBedrooms}
-          name="minBedrooms"
-          onChange={handleChange}
+          options={roomOptions}
+          value={selectedRoom}
+          isClearable
+          isSearchable={false}
+          placeholder="Rooms"
+          classNames={{
+            control: () => "p-1.5 text-sm rounded-lg",
+          }}
+          className="w-full"
+          styles={{
+            input: (base) => ({
+              ...base,
+              "input:focus": {
+                boxShadow: "none",
+              },
+            }),
+            control: (base, state) => ({
+              ...base,
+              border: state.isFocused ? 0 : 0,
+              boxShadow: state.isFocused ? 0 : 0,
+              "&:hover": {
+                border: state.isFocused ? 0 : 0,
+              },
+            }),
+          }}
+          theme={(theme) => ({
+            ...theme,
+            borderRadius: "1",
+            colors: {
+              ...theme.colors,
+              primary25: "#7dd3fc",
+              primary: "black",
+            },
+          })}
+          onChange={handleRoomChange}
           required
         />
       </div>
