16d0b740fc79102bd8f142bda221a2447d4cf037
yigitaksoy <yigitaksoy@gmail.com>
2023-06-15 16:43:58 +0200

    Add price data and replace inputs with react-select

--- client/src/components/PriceInput.jsx
+++ client/src/components/PriceInput.jsx
@@ -1,33 +1,122 @@
+import Select from "react-select";
+import priceRental from "../data/priceRental.json";
+import priceSale from "../data/priceSale.json";
+
 const PriceInput = ({ handleChange, formData }) => {
+  const { listingType, minPrice, maxPrice } = formData;
+
+  const options = listingType === "huur" ? priceRental : priceSale;
+
+  const minPriceOptions = options;
+  const maxPriceOptions = options.filter((option) => option.value >= minPrice);
+
+  const selectedMinPrice = minPriceOptions.find(
+    (option) => option.value === minPrice
+  );
+  const selectedMaxPrice = maxPriceOptions.find(
+    (option) => option.value === maxPrice
+  );
+
+  const handleMinPriceChange = (selectedOption) => {
+    const event = {
+      target: {
+        name: "minPrice",
+        value: selectedOption ? selectedOption.value : "",
+      },
+    };
+    handleChange(event);
+  };
+
+  const handleMaxPriceChange = (selectedOption) => {
+    const event = {
+      target: {
+        name: "maxPrice",
+        value: selectedOption ? selectedOption.value : "",
+      },
+    };
+    handleChange(event);
+  };
   return (
     <div className="grid grid-cols-2 gap-2 p-2">
       <div className="relative flex items-center">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          <p>From</p>
-        </div>
-        <input
-          type="number"
+        <Select
           id="minPrice"
-          className="input block w-full rounded-lg border p-2.5 pl-16 text-sm"
-          placeholder="Price From"
-          value={formData.minPrice}
-          name="minPrice"
-          onChange={handleChange}
+          options={minPriceOptions}
+          value={selectedMinPrice}
+          isClearable
+          isSearchable={false}
+          placeholder="Min Price"
+          classNames={{
+            control: () => "p-1.5 text-sm rounded-lg",
+          }}
+          className="w-full"
+          styles={{
+            input: (base) => ({
+              ...base,
+              "input:focus": {
+                boxShadow: "none",
+              },
+            }),
+            control: (base, state) => ({
+              ...base,
+              border: state.isFocused ? 0 : 0,
+              boxShadow: state.isFocused ? 0 : 0,
+              "&:hover": {
+                border: state.isFocused ? 0 : 0,
+              },
+            }),
+          }}
+          theme={(theme) => ({
+            ...theme,
+            borderRadius: "1",
+            colors: {
+              ...theme.colors,
+              primary25: "#7dd3fc",
+              primary: "black",
+            },
+          })}
+          onChange={handleMinPriceChange}
           required
         />
       </div>
       <div className="relative flex items-center">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          <p>To</p>
-        </div>
-        <input
-          type="number"
-          id="maxPrice"
-          className="input block w-full rounded-lg border p-2.5 pl-10 text-sm"
-          placeholder="Limit"
-          value={formData.maxPrice}
-          name="maxPrice"
-          onChange={handleChange}
+        <Select
+          id="minPrice"
+          options={maxPriceOptions}
+          value={selectedMaxPrice}
+          isClearable
+          isSearchable={false}
+          placeholder="Max Price"
+          className="w-full"
+          classNames={{
+            control: () => "p-1.5 text-sm rounded-lg",
+          }}
+          styles={{
+            input: (base) => ({
+              ...base,
+              "input:focus": {
+                boxShadow: "none",
+              },
+            }),
+            control: (base, state) => ({
+              ...base,
+              border: state.isFocused ? 0 : 0,
+              boxShadow: state.isFocused ? 0 : 0,
+              "&:hover": {
+                border: state.isFocused ? 0 : 0,
+              },
+            }),
+          }}
+          theme={(theme) => ({
+            ...theme,
+            borderRadius: "1",
+            colors: {
+              ...theme.colors,
+              primary25: "#7dd3fc",
+              primary: "black",
+            },
+          })}
+          onChange={handleMaxPriceChange}
           required
         />
       </div>
