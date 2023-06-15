11ae2225b9b5c298400521ec76a735184465fb93
yigitaksoy <yigitaksoy@gmail.com>
2023-06-15 18:22:39 +0200

    Replace surface input with react-select

--- client/src/components/SizeAndRoomsInput.jsx
+++ client/src/components/SizeAndRoomsInput.jsx
@@ -1,16 +1,61 @@
+import Select from "react-select";
+import surface from "../data/surface.json";
+
 const SizeAndRoomsInput = ({ handleChange, formData }) => {
+  const options = surface;
+  const selectedSurface = options.find(
+    (option) => option.value === formData.minSize
+  );
+
+  const handleSurfaceChange = (selectedOption) => {
+    const event = {
+      target: {
+        name: "minSize",
+        value: selectedOption ? selectedOption.value : "",
+      },
+    };
+    handleChange(event);
+  };
   return (
     <div className="grid grid-cols-2 gap-2 p-2">
       <div className="relative flex items-center">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"></div>
-        <input
-          type="number"
+        <Select
           id="minSize"
-          className="input block w-full rounded-lg border p-2.5 pl-16 text-sm"
-          placeholder="Size"
-          value={formData.minSize}
-          name="minSize"
-          onChange={handleChange}
+          options={options}
+          value={selectedSurface}
+          isClearable
+          isSearchable={false}
+          placeholder="Surface"
+          classNames={{
+            control: () => "p-1.5 text-sm rounded-lg",
+          }}
+          className="w-full"
+          styles={{
+            input: (base) => ({
+              ...base,
+              "input:focus": {
+                boxShadow: "none",
+              },
+            }),
+            control: (base, state) => ({
+              ...base,
+              border: state.isFocused ? 0 : 0,
+              boxShadow: state.isFocused ? 0 : 0,
+              "&:hover": {
+                border: state.isFocused ? 0 : 0,
+              },
+            }),
+          }}
+          theme={(theme) => ({
+            ...theme,
+            borderRadius: "1",
+            colors: {
+              ...theme.colors,
+              primary25: "#7dd3fc",
+              primary: "black",
+            },
+          })}
+          onChange={handleSurfaceChange}
           required
         />
       </div>
