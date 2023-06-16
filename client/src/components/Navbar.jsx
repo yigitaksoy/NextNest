5793823616259aae3b21051e718422be6712a75a
yigitaksoy <yigitaksoy@gmail.com>
2023-06-16 19:08:33 +0200

    Remove logout icon and change font family

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { AuthContext } from "../context/AuthContext";
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import NextNest from "../assets/images/nextnest-white.png";
-import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
 
 const Navbar = () => {
   const { currentUser } = useContext(AuthContext);
@@ -12,14 +11,15 @@ const Navbar = () => {
       <div className="flex-1">
         <img src={NextNest} alt="nextnest-logo" className="w-22 h-10" />
       </div>
-      <div className="mx-auto text-lg text-gray-300">
+      <div className="mx-auto text-lg text-white">
         <button
           className="btn-ghost btn-square btn mx-auto flex"
           onClick={() => signOut(auth)}
         >
           <div className="mr-10 flex items-center">
-            <p className="mr-2 text-white">Logout</p>
-            <ArrowRightOnRectangleIcon className="h-5 w-5 text-white" />
+            <p className="mr-2 font-nove text-white hover:text-sky-300">
+              Logout
+            </p>
           </div>
         </button>
       </div>
