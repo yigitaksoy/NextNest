58e785b3fdf363d4a10c99d42aefa29a97b0127d
yigitaksoy <yigitaksoy@gmail.com>
2023-06-18 23:10:10 +0200

    Hide search bar box shadow on smaller devices

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -105,7 +105,11 @@ const SearchBar = () => {
   return (
     <div className="font-fontDegular">
       <div className="container mx-auto flex h-screen items-center justify-center p-2 md:p-0 lg:w-2/3">
-        <form onSubmit={handleSubmit} className="w-screen" id="searchbar">
+        <form
+          onSubmit={handleSubmit}
+          className="lg:shadow-searchBar w-screen"
+          id="searchbar"
+        >
           <div className="grid grid-cols-1 rounded-xl  bg-gray-50 p-6  md:gap-2">
             <ListingTypeInput handleChange={handleChange} formData={formData} />
             <LocationInput handleChange={handleChange} formData={formData} />
