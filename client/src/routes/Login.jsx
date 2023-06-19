ed93db05a52ce52926f247879cc6c9508678d841
yigitaksoy <yigitaksoy@gmail.com>
2023-06-19 16:04:33 +0200

    Add drop shadow for login form

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -80,14 +80,17 @@ const Login = () => {
   };
 
   return (
-    <section className="h-screen bg-sky-400">
-      <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
+    <section className="h-screen bg-sky-300">
+      <div className="mx-auto flex flex-col items-center justify-center px-6 py-8  md:h-screen lg:py-0">
         <Link to="/" className="mb-6">
           <img src={NextNest} alt="nextnest-logo" className="w-22 h-10" />
         </Link>
-        <div className="w-full rounded-lg bg-white font-degular shadow sm:max-w-md md:mt-0 xl:p-0">
+        <div
+          id="login"
+          className=" w-full rounded-lg bg-white font-degular sm:max-w-md md:mt-0 md:shadow-searchBar xl:p-0"
+        >
           <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
-            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+            <h1 className="text-center font-marker text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
               Welcome back!
             </h1>
             <div className="text-center text-sm text-red-500">
@@ -138,7 +141,7 @@ const Login = () => {
               </div>
               <button
                 type="submit"
-                className="w-full rounded-lg bg-black px-5 py-2.5  text-center text-sm font-medium text-white hover:bg-neutral-800  focus:outline-none focus:ring-4 focus:ring-violet-300"
+                className="w-full rounded-lg bg-black px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-neutral-800 focus:outline-none focus:ring-4 focus:ring-violet-300"
               >
                 Sign in
               </button>
