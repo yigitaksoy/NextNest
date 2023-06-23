41eaacabcb0258aceb985838f944ec93501fc702
yigitaksoy <yigitaksoy@gmail.com>
2023-06-24 01:28:59 +0200

    Update city input to use react-select for location options

--- client/src/components/LocationInput.jsx
+++ client/src/components/LocationInput.jsx
@@ -1,20 +1,65 @@
+import Select from "react-select";
+import municipalities from "../data/municipalities.json";
 import { MapIcon } from "@heroicons/react/24/outline";
 
 const LocationInput = ({ handleChange, formData }) => {
+  const locationOptions = municipalities;
+
+  const selectedLocation = locationOptions.find(
+    (option) => option.value === formData.location
+  );
+
+  const handleLocationChange = (selectedLocation) => {
+    const event = {
+      target: {
+        name: "location",
+        value: selectedLocation ? selectedLocation.value : "",
+      },
+    };
+    handleChange(event);
+  };
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4">
       <div className="p-2">
         <div className="relative">
-          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-            <MapIcon className="h-5 w-5 text-gray-500" />
-          </div>
-          <input
-            type="text"
-            className="search-input  input block w-full rounded-lg border border-gray-300 p-2.5 pl-10 text-custom-black focus:border-sky-300 focus:outline-none focus:ring-0 sm:text-sm"
+          <Select
+            id="location"
+            options={locationOptions}
+            value={selectedLocation}
+            isClearable
             placeholder="City, neighborhood, address, etc."
-            value={formData.location}
-            name="location"
-            onChange={handleChange}
+            noOptionsMessage={() => "Nothing to show"}
+            classNames={{
+              control: () => "p-1.5 text-sm rounded-xl",
+            }}
+            className="search-input w-full"
+            styles={{
+              input: (base) => ({
+                ...base,
+                "input:focus": {
+                  boxShadow: "none",
+                },
+              }),
+              control: (base, state) => ({
+                ...base,
+                border: state.isFocused ? 0 : 0,
+                boxShadow: state.isFocused ? 0 : 0,
+                "&:hover": {
+                  border: state.isFocused ? 0 : 0,
+                },
+              }),
+            }}
+            theme={(theme) => ({
+              ...theme,
+              borderRadius: "1",
+              colors: {
+                ...theme.colors,
+                primary25: "#7dd3fc",
+                primary: "black",
+              },
+            })}
+            onChange={handleLocationChange}
             required
           />
         </div>
