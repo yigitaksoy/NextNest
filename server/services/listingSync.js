fad7630b40b1623e00804039a02ac0a29b37ffa9
yigitaksoy <yigitaksoy@gmail.com>
2023-06-27 01:12:19 +0200

    Create cron job for listing synchronization

---
+++ server/services/listingSync.js
@@ -0,0 +1,8 @@
+const cron = require("node-cron");
+const { syncListings } = require("../controllers/apiController");
+
+cron.schedule("*/30 * * * *", async function () {
+  console.log("Running a job every 15 minutes.");
+  await syncListings();
+  console.log("Done! Thank you for using NextNest! 🚀");
+});
