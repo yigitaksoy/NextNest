7b486e2aab166e823cd6bf69e1cf172dcb27ad9d
yigitaksoy <yigitaksoy@gmail.com>
2023-06-26 14:09:24 +0200

    Remove database console log

--- server/config/database.js
+++ server/config/database.js
@@ -8,8 +8,6 @@ const connectToDatabase = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
-    console.log("Connected to the database");
   } catch (error) {
     console.error("Database connection error:", error.message);
     process.exit(1);
