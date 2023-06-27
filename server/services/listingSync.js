4adccf38d799929752deeb87a479e5e7ada6c606
yigitaksoy <yigitaksoy@gmail.com>
2023-06-27 19:03:28 +0200

    Fix cron job comment and run every 5 minutes

--- server/services/listingSync.js
+++ server/services/listingSync.js
@@ -1,7 +1,7 @@
 const cron = require("node-cron");
 const { syncListings } = require("../controllers/apiController");
 
-// Schedule the job to run every 15 minutes between 7 am and 2 am
+// Schedule the job to run every 5 minutes between 7 am and 2 am
 cron.schedule("*/5 7-23,0 * * *", async function () {
   console.log("Running a job every 15 minutes between 7 am and 2 am.");
   await syncListings();
