81e533bfdbfb7006f49f2de9d0ccf7dc4ea18f88
yigitaksoy <yigitaksoy@gmail.com>
2023-06-27 18:28:46 +0200

    Handle IP lookup errors for proxy

--- server/services/listingService.js
+++ server/services/listingService.js
@@ -43,8 +43,12 @@ const scrapeListings = async (url, listingType) => {
 
     await useProxy(page, process.env.PROXY);
 
-    const data = await useProxy.lookup(page);
-    console.log("Page IP:", data.ip);
+    try {
+      const data = await useProxy.lookup(page);
+      console.log("Page IP:", data.ip);
+    } catch (error) {
+      console.log("Failed to look up the proxy IP:", error);
+    }
 
     while (true) {
       console.log("Scraping page:", currentPage);
