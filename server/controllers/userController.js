29e04dea8f43093fb3ae1ae0b94ae4da8f89d346
yigitaksoy <yigitaksoy@gmail.com>
2023-06-29 02:57:07 +0200

    Remove unnecessary debug statements

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -15,7 +15,7 @@ exports.register = async (req, res) => {
       subscription: true,
     });
 
-    res.status(201).json({ message: "User registered successfully" });
+    res.status(201).json({ message: "User registered successfully!" });
   } catch (error) {
     console.error("Error registering user:", error);
     res.status(500).json({ error: "An error occurred while registering user" });
@@ -26,7 +26,6 @@ exports.register = async (req, res) => {
 exports.getUserSearch = async (req, res, next) => {
   try {
     const user = req.user;
-    console.log("FETCH User information:", user); // Debug statement
 
     // Check if the user is authenticated
     if (!user) {
@@ -51,11 +50,10 @@ exports.getUserSearch = async (req, res, next) => {
 exports.saveUserSearch = async (req, res, next) => {
   try {
     const user = req.user;
-    console.log("SAVE User information:", user); // Debug statement
 
     // Check if the user is authenticated
     if (!user) {
-      return res.status(401).json({ message: "User not authenticated" });
+      return res.status(401).json({ message: "User not authenticated!" });
     }
 
     const {
