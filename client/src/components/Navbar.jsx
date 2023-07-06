42a4d025ce7e282fd794351f0e4f54657e023420
yigitaksoy <yigitaksoy@gmail.com>
2023-07-06 13:12:09 +0200

    Set navbar to sticky on all screen sizes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import NextNest from "../assets/images/nextnest-white.png";
 const Navbar = () => {
   const { currentUser } = useContext(AuthContext);
   return (
-    <div className="navbar sticky top-0 z-50 bg-black p-5 md:fixed">
+    <div className="navbar sticky top-0 z-50 bg-black p-5">
       <div className="flex-1">
         <img src={NextNest} alt="nextnest-logo" className="w-22 h-10" />
       </div>
