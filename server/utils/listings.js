5caa81666af9e14a584bfa67a0999e7f44535faa
yigitaksoy <yigitaksoy@gmail.com>
2023-07-10 17:07:29 +0200

    Update listing selectors and handle cookie consent

--- server/utils/listings.js
+++ server/utils/listings.js
@@ -11,52 +11,108 @@ const fetchAndExtractListings = async (page, url) => {
     console.log("Navigation failed:", error);
   }
 
-  let rawListings;
   try {
-    rawListings = await page.evaluate(() => {
-      const extractListingDetails = (element) => {
-        const image =
-          element?.querySelector(".search-result-image img")?.src ||
-          element?.querySelector(".promo-thumbnail img")?.src;
-        const title = element
-          ?.querySelector(".search-result__header-title")
-          ?.textContent.trim();
-        const linkElement = element.querySelector(
-          "a[data-object-url-tracking='resultlist']"
+    let consentDialogFound = true;
+    while (consentDialogFound) {
+      try {
+        await page.waitForSelector(
+          ".banner-actions-container #onetrust-accept-btn-handler",
+          { timeout: 5000 }
         );
-        const url = linkElement?.getAttribute("href")?.replace(/(\?.*)$/, "");
-        const postal_code = element
-          ?.querySelector(".search-result__header-subtitle")
-          ?.textContent.trim();
-        const price = (
-          element?.querySelector(".search-result-price")?.textContent.trim() ||
-          ""
-        ).replace(/\s*k\.k\.\s*$/, "");
-        const details = Array.from(
-          element?.querySelectorAll(".search-result-kenmerken li")
-        ).map((li) => li.textContent.trim());
 
-        return {
-          image,
-          title,
-          url:
-            url && url.startsWith("https://www.funda.nl")
-              ? url
-              : `https://www.funda.nl${url}`,
-          postal_code,
-          price,
-          details,
-        };
-      };
+        const navigationPromise = page.waitForNavigation();
+        await page.click(
+          ".banner-actions-container #onetrust-accept-btn-handler"
+        );
+        await navigationPromise;
 
-      const elements = Array.from(document.querySelectorAll(".search-result"));
-      return elements.map(extractListingDetails);
-    });
+        console.log("Accepted cookies");
+      } catch (error) {
+        consentDialogFound = false;
+      }
+    }
   } catch (error) {
-    console.error("⛔ Error during page evaluation:", error);
+    console.log("Did not find cookie consent dialog");
   }
 
-  return rawListings;
+  const searchResultItemsExist = await page.evaluate(() => {
+    const searchResultItems = document.querySelectorAll(
+      '[data-test-id="search-result-item"]'
+    );
+    return searchResultItems.length > 0;
+  });
+
+  if (searchResultItemsExist) {
+    let rawListings;
+    try {
+      rawListings = await page.evaluate(() => {
+        const extractListingDetails = (element) => {
+          const imageElement = element?.querySelector("img");
+          const imageSrcSet = imageElement?.getAttribute("srcset");
+          const image = imageSrcSet
+            ? imageSrcSet.match(/https:\/\/[^\s]+.jpg/g)?.pop()
+            : null;
+
+          const titleElement = element.querySelector(
+            'h2[data-test-id="street-name-house-number"]'
+          );
+          const title = titleElement?.textContent.trim();
+
+          const linkElement = titleElement?.closest("a");
+          const url = linkElement?.getAttribute("href")?.replace(/(\?.*)$/, "");
+
+          const postal_code = element
+            ?.querySelector('div[data-test-id="postal-code-city"]')
+            ?.textContent.trim();
+
+          let price = "";
+          const rentPriceElement = element?.querySelector(
+            'p[data-test-id="price-rent"]'
+          );
+          const salePriceElement = element?.querySelector(
+            'p[data-test-id="price-sale"]'
+          );
+          if (rentPriceElement) {
+            price = rentPriceElement.textContent
+              .trim()
+              .replace(/\s*p\/mo\.\s*$/, "");
+          } else if (salePriceElement) {
+            price = salePriceElement.textContent.trim() || "";
+            price = price.replace(/\s*k\.k\.\s*$/, "");
+          }
+
+          const details = Array.from(
+            element?.querySelectorAll(".search-result-kenmerken li")
+          ).map((li) => li.textContent.trim());
+
+          return {
+            image,
+            title,
+            url:
+              url && url.startsWith("https://www.funda.nl")
+                ? url
+                : `https://www.funda.nl${url}`,
+            postal_code,
+            price,
+            details,
+          };
+        };
+
+        const elements = Array.from(
+          document.querySelectorAll('[data-test-id="search-result-item"]')
+        );
+
+        return elements.map(extractListingDetails);
+      });
+    } catch (error) {
+      console.error("⛔ Error during page evaluation:", error);
+    }
+
+    return rawListings;
+  } else {
+    console.log("No search result items found.");
+    return [];
+  }
 };
 
 module.exports = { fetchAndExtractListings };
