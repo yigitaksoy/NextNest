4883b6e25688463837de0468f1c841e90cb04db9
yigitaksoy <yigitaksoy@gmail.com>
2023-07-11 17:36:41 +0200

    Extract the number of bedrooms from the listing

--- server/services/listingDetails.js
+++ server/services/listingDetails.js
@@ -60,6 +60,15 @@ const listingDetails = async (page, url, listingType) => {
                   .map((str) => str.trim())
                   .join(" ")
               : undefined;
+
+            // Extract the number of bedrooms
+            if (label === "Number of rooms") {
+              value = value.replace(
+                /^.+?\((\d+)\s+bedrooms?\)/i,
+                "$1 bedrooms"
+              );
+            }
+
             details[label] = value;
           }
         });
