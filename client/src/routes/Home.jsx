29b86707accde55f540de45b66298284deabb99c
yigitaksoy <yigitaksoy@gmail.com>
2023-07-12 01:12:07 +0200

    Change home route background color

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-    <div className="bg-gray-300">
+    <div className="bg-gray-100">
       <Navbar />
       <div className="md:h-auto md:mb-10">
         <SearchBar />
