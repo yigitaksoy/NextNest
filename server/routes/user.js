9dee35accec7be4e4992c66ecbad9d0120d926ae
yigitaksoy <yigitaksoy@gmail.com>
2023-07-12 10:11:21 +0200

    Add functionality to allow users to unsubscribe from receving new listings

--- server/routes/user.js
+++ server/routes/user.js
@@ -10,5 +10,6 @@ router.post("/register", userController.register);
 router.use(verifyToken(admin));
 router.get("/search", userController.getUserSearch);
 router.post("/search", userController.saveUserSearch);
+router.put("/subscription", userController.updateSubscription);
 
 module.exports = router;
