e2f0f70ca574f371e0c8fb934859e4c814b1d5c8
yigitaksoy <yigitaksoy@gmail.com>
2023-07-19 23:48:02 +0200

    Restyle listing type buttons

--- client/src/components/ListingTypeInput.jsx
+++ client/src/components/ListingTypeInput.jsx
@@ -1,7 +1,7 @@
 const ListingTypeInput = ({ handleChange, formData }) => {
   return (
     <div className="flex flex-shrink-0 font-marker">
-      <div className="relative bg-transparent p-2" id="listing-button">
+      <button className="bg-transparent p-2 listing-button" type="button">
         <input
           className="hidden"
           type="radio"
@@ -13,14 +13,14 @@ const ListingTypeInput = ({ handleChange, formData }) => {
           required
         />
         <label
-          className="btn cursor-pointer border-none bg-transparent px-5 py-2.5 text-black"
+          className="btn border-2 bg-transparent px-5 py-2.5 text-black"
           htmlFor="forRent"
         >
           For Rent
         </label>
-      </div>
+      </button>
 
-      <button className="bg-transparent p-2" type="button" id="listing-button">
+      <button className="bg-transparent p-2 listing-button" type="button">
         <input
           className="hidden"
           type="radio"
